c64c23dfa893533f57e7b76fb96fa93685a20795
fabio0604 <129704222+fabio0604@users.noreply.github.com>
2024-10-10 08:56:38 -0300

    versão 4

---
+++ script.js
@@ -0,0 +1,26 @@
+<script>
+let slideIndex = 0;
+showSlides();
+
+function plusSlides(n) {
+    slideIndex += n;
+    showSlides();
+}
+
+function showSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    if (slideIndex >= slides.length) { slideIndex = 0; }
+    if (slideIndex < 0) { slideIndex = slides.length - 1; }
+    
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slides[slideIndex].style.display = "block";  
+}
+
+// Mudar automaticamente a imagem a cada 5 segundos
+setInterval(function() {
+    slideIndex++;
+    showSlides();
+}, 5000);
+</script>
